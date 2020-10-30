538c066582f4af8b43e6591559c0afee3efd224c
hwk <1104424681@qq.com>
2020-10-30 09:53:59 +0800

    用户中心增加用户上传头像功能

--- src/main/java/com/it/controller/userMenu/UserMenuController.java
+++ src/main/java/com/it/controller/userMenu/UserMenuController.java
@@ -1,17 +1,25 @@
 package com.it.controller.userMenu;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.annotation.Resource;
 
 import org.apache.shiro.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.it.po.UserInfo;
 import com.it.service.systemManagement.UserService;
+import com.it.util.Result;
 
 /**
  * 
@@ -27,6 +35,13 @@ public class UserMenuController {
 	private static final Logger log = LoggerFactory.getLogger(UserMenuController.class);
 	@Resource
 	private UserService userService;
+	//静态资源访问位置
+	@Value("${file.staticAccessPath}")
+	private String staticAccessPath;
+	//图像上传位置
+	@Value("${file.uploadUserImagePath}")
+	private String uploadUserImagePath;
+	
 	
 	@RequestMapping("/userCenter")
 	public ModelAndView initPage(Model model) {
@@ -42,7 +57,48 @@ public class UserMenuController {
 		modelAndView.addObject("phone", user.getPhone());
 		modelAndView.addObject("email", user.getEmail());
 		modelAndView.addObject("sex", user.getSex());
-//		modelAndView.addObject("sex", user.getSex() == 0 ? "男" : "女");
+		modelAndView.addObject("image", user.getImage());
 		return modelAndView;
+	}
+	
+	/**
+	 * 
+	 * @Title: uploadImage  
+	 * @Description: TODO  用户上传图像
+	 * @param @param file
+	 * @param @return    参数  
+	 * @return Result    返回类型  
+	 * @throws
+	 */
+	@RequestMapping("/uploadImage")
+	@ResponseBody
+	public Result uploadImage(@RequestParam("file_data") MultipartFile file) {
+		if(file.isEmpty()) {
+			log.error("上传文件不存在！");
+			return Result.error();
+		}
+		UserInfo user = (UserInfo) SecurityUtils.getSubject().getPrincipal();
+		String fileName = file.getOriginalFilename();
+		String newFileName = user.getName() + fileName.substring(fileName.indexOf("."));
+		
+		File f = new File(uploadUserImagePath + newFileName);
+		if(!f.getParentFile().exists()) {
+			f.getParentFile().mkdirs();
+		}
+		try {
+			file.transferTo(f);
+		} catch (IllegalStateException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+		} catch (IOException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+		}
+		//更新数据库用户头像
+		UserInfo newUser = new UserInfo();
+		newUser.setId(user.getId());
+		newUser.setImage("/static/uploadfile/userImage/"+newFileName);
+		userService.update(newUser);
+		return Result.success();
 	}
 }
